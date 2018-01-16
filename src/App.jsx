0335a63eeec249cf2c4ada012d17ab864a23b895
Faye Jakovcevic <fayelouise.jakovich@gmail.com>
2018-01-16 14:29:44 -0700

    geolocation working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Map from './comps/Map/Map';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <Map/>
       </div>
     );
   }
