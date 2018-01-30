7e509ded677f07573402702908fba9448af3e0c6
nathan-08 <nathanryan001@gmail.com>
2018-01-30 10:09:46 -0700

    getting distance on requestList reliably; needed to get reqeustArr before running this.distance()

--- src/comps/RequestList/RequestList.jsx
+++ src/comps/RequestList/RequestList.jsx
@@ -16,11 +16,9 @@ import './RequestList.css'
 import { RaisedButton } from 'material-ui'
 import { lightBlue500 } from 'material-ui/styles/colors';
 
-
-
 class RequestList extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             requestArr: [],
             clientID: null
@@ -28,54 +26,57 @@ class RequestList extends Component {
     }
 
     //Get the Geolocation of the user
-    componentDidMount() {
-        axios.get('auth/me').then(res=>{this.setState({clientID:res.data.user})})
-        if (navigator.geolocation) {
-            // console.log('supported in browser')
-            navigator.geolocation.getCurrentPosition((position) => {
-                const lat = position.coords.latitude;
-                const lng = position.coords.longitude;
-                this.props.setLocationState(lat, lng)
-            })
-        }
-        else {
-            // console.log('not supported in browser')
-        }
-        axios.get('/request').then((res) => {
+    async componentDidMount() {
+        await axios.get('/request').then((res) => { //get request array
             this.setState({
                 requestArr: res.data
             })
         })
+        axios.get('auth/me').then(res=>{
+            this.setState({ clientID:res.data.user }) })
+
+        
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const lat = position.coords.latitude;
+                const lng = position.coords.longitude;
+                this.props.setLocationState(lat, lng)
+                this.distance(lat, lng)
+                // console.log('lat, lng:',lat,lng)
+            })
+        }
+        else {
+            console.log('not supported in browser')
+        }
+        
     }
 
     componentWillReceiveProps(nextprops) {
         //Calc distance and push to requestArr
         const lat = nextprops.lat
         const lng = nextprops.lng 
-        this.distance(lat, lng)
-        // console.log('myLocation', lat, lng)
+        this.distance(lat, lng)  // *********
     }
 
     distance = (lat1, lon1) => {
+        console.log('called this.distance---------------------')
+        console.log('this.state.requestArr: ',this.state.requestArr)
         let arr = this.state.requestArr
-        // console.log('arr', arr)
         let newArr = []
         for (var i = 0; i < arr.length; i++) {
-            // console.log('req lat', arr[i].lat, 'req long',arr[i].long)
-            // console.log('user lat', lat1, 'user long',lon1)
             let type = 'imperial'
             //const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=${type}&origins=${lat1},${lon1}&destinations=${arr[i].lat},${arr[i].long}&key=AIzaSyCIIg2weQK6p4wUTy6nXrCj4-hPGgA40xI`
-            //console.log(url) 
+           
             //newArr.push(axios.get(url))
+            console.log('making axios call-----------------------------------------------------')
             newArr.push(axios.put('/getDistance',{type:type, lat1: lat1, lon1: lon1, lat2:arr[i].lat, lon2: arr[i].long}))
         }
-        // console.log('array of promise:', newArr)
+      
         Promise.all(newArr).then(res => {
             let requestArr = this.state.requestArr
             let newState = {}
             for (var j = 0; j < requestArr.length; j++) {
                 requestArr[j].distance = res[j].data.rows[0].elements[0].distance.text
-                // console.log(`distance for index ${j}`,res[j].data.rows[0].elements[0].distance.text)
             }
             this.setState({requestArr})
         })
